4b3eb1f43e58851f338d87e4a882581e6e510891
Karthika1010 <151449249+Karthika1010@users.noreply.github.com>
2023-12-14 14:27:04 +0545

    Update JavaProgram.java

--- PullPrograms/src/main/java/studyjava/JavaProgram.java
+++ PullPrograms/src/main/java/studyjava/JavaProgram.java
@@ -5,6 +5,7 @@ public class JavaProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello java");
+System.out.println("Good morning");		
 	}
 
 }
