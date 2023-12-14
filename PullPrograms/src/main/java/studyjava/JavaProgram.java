ca7e35df041dc65e13c01635f35a351bb7babc18
HP <HP@DESKTOP-LJM4THQ>
2023-12-14 15:05:31 +0545

    changes

--- PullPrograms/src/main/java/studyjava/JavaProgram.java
+++ PullPrograms/src/main/java/studyjava/JavaProgram.java
@@ -5,7 +5,8 @@ public class JavaProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello java");
-System.out.println("Good morning");		
+System.out.println("Good morning");	
+System.out.println("Good night");	
 	}
 
 }
